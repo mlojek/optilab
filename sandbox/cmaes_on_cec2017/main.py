0e6cb03943805a56042c2921eca016468c5e9242
mlojek <mlojek@protonmail.com>
2024-07-08 16:40:57 +0200

    run cmaes on cec function

--- sandbox/cmaes_on_cec2017/main.py
+++ sandbox/cmaes_on_cec2017/main.py
@@ -1,9 +1,23 @@
-from cec2017.functions import f1
+import cma
 import matplotlib.pyplot as plt
+import numpy as np
+from cec2017.functions import f1
+
+
+def run_cmaes_on_cec(cec_function: callable, dims: int):
+    '''
+    TODO
+    '''
+    x_start = np.random.normal(size=(1, dims))
+    print(x_start)
+    x_start = [0 for _ in range(dims)]
+    xopt, es = cma.fmin2(
+        lambda x: cec_function([x]),
+        x_start,
+        1.0
+    )
+    # TODO how to plot the results????
 
 
 if __name__ == '__main__':
-    x = [i for i in range(1000)]
-    y = [f1([[xval, xval]]) for xval in x]
-    plt.plot(x, y)
-    plt.show()
+    run_cmaes_on_cec(f1, 2)

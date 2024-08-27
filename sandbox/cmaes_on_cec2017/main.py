9b24d4a7bda840b2b85f189c03fca369bdd9dffa
mlojek <mlojek@protonmail.com>
2024-08-27 16:16:59 +0200

    working cmaes on cec demo with saving results to json

--- sandbox/cmaes_on_cec2017/main.py
+++ sandbox/cmaes_on_cec2017/main.py
@@ -1,29 +1,85 @@
+from typing import Tuple
+import json
+
 import cma
-import matplotlib.pyplot as plt
 import numpy as np
-from cec2017.functions import all_functions
-from cec2017.functions import *
+from tqdm import tqdm
+
+from config import MAX_FES, FUNCTIONS, NUM_RUNS
 
 
-def run_cmaes_on_cec(cec_function: callable, dims: int):
-    '''
-    TODO
-    '''
-    # 4 + 3sqrt(dim)
-    # 4dim
-    x_start = np.random.normal(size=(4*dims))
-    print(x_start)
-    # x_start = [0 for _ in range(dims)]
-    # TODO ograniczenia kostkowe - odbijanie Lamarck'a
-    xopt, es = cma.fmin2(
-        lambda x: cec_function([x]),
-        x_start,
-        1.0
+def run_cmaes_on_cec(
+    cec_function: callable,
+    dims: int,
+    population_size: int,
+    call_budget: int,
+    constraints: Tuple[float, float],
+):
+    """
+    Runs the CMA-ES algorithm on a CEC function.
+    
+    :param cec_function: The CEC benchmark function to optimize.
+    :param dims: The dimensionality of the problem.
+    :param population_size: The population size for the CMA-ES algorithm.
+    :param call_budget: The maximum number of function evaluations.
+    :param constraints: The lower and upper bounds for the search space.
+    :return: The best solution found by the CMA-ES algorithm.
+    """
+    x0 = np.random.uniform(low=constraints[0], high=constraints[1], size=dims)
+    # print(x0)
+    # exit(0)
+    sigma0 = 0.3
+    
+    es = cma.CMAEvolutionStrategy(
+        x0, 
+        sigma0, 
+        {
+            'popsize': population_size, 
+            'bounds': [constraints[0], constraints[1]], 
+            'maxfevals': call_budget
+        }
     )
-    # cma.plot()
-    # cma.s.figsave('cmaes_result.png')
+    # TODO does the cmaes stick to bounds and maxfes by itself?
+
+    while not es.stop():
+        solutions = es.ask()
+        # TODO odbijanie lamarcka
+        # TODO better call budget tracking
+        fitness_values = [cec_function([x.tolist()])[0] for x in solutions]
+
+        if es.result.evaluations >= call_budget:
+            break
+
+        es.tell(solutions, fitness_values)
+
+    return es.result.fbest
 
 
-if __name__ == '__main__':
-    # for f in all_functions:
-    run_cmaes_on_cec(f8, 10)
+# 4 + 3sqrt(dim)
+# 4dim
+if __name__ == "__main__":
+    results = {}
+
+    for name, function, dimensions, constraints in FUNCTIONS:
+        for dimension in dimensions:
+            print(f"{name} dim {dimensions}")
+            maxes = [
+                run_cmaes_on_cec(
+                    function,
+                    dimension,
+                    4 * dimension,
+                    MAX_FES * dimension,
+                    constraints
+                )
+                for _ in tqdm(range(NUM_RUNS))
+            ]
+            try:
+                results[name][str(dimension)] = maxes
+            except KeyError:
+                results[name] = {
+                    str(dimension): maxes
+                }
+        break
+
+    with open("results.json", "w") as results_handle:
+        json.dump(results, results_handle)

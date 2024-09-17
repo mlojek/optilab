353bf34f94387461bb7b8ddc5c78c7de8d7433dd
Marcin <mlojek@protonmail.com>
2024-09-17 20:00:05 +0200

    start experiment results class implementation

---
+++ src/experiment_results.py
@@ -0,0 +1,56 @@
+'''
+Script docstring
+'''
+from typing import Dict, List, Union
+
+
+class ExperimentResults:
+    '''
+    TODO class docstring
+    '''
+    def __init__(self):
+        '''
+        TODO
+        '''
+        # TODO metadata
+        # TODO log
+        # TODO result
+        pass
+
+    def plot_ecdf(self):
+        '''
+        TODO
+        '''
+        pass
+
+    def to_dict(self):
+        '''
+        TODO
+        '''
+        pass
+
+
+class ExperimentResultsCollection:
+    '''
+    TODO
+    '''
+    def __init__(self):
+        '''
+        TODO
+        '''
+        pass
+
+    def save_to_file(self):
+        '''
+        TODO
+        TODO determine csv or json by file extension
+        '''
+        pass
+
+    # TODO for the aggregate class
+    def plot_box_plot(self):
+        '''
+        TODO
+        '''
+        # TODO specify how many boxes per plot and generate multiple plots
+        pass

ca4182b3f12c74cb9a1189eacc9ce94c72aa326b
mlojek <mlojek@protonmail.com>
2024-10-25 12:38:49 +0200

    add dummy unit tests

---
+++ tests/test_dummy.py
@@ -0,0 +1,14 @@
+'''
+Dummy unit tests that serve as placeholders before any real tests will be implemented.
+'''
+
+
+class TestDummy:
+    '''
+    Dummy unit tests.
+    '''
+    def test_dummy(self):
+        '''
+        Dummy unit test.
+        '''
+        assert True

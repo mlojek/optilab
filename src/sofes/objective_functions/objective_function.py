cf74de139a2e1d3359b68104c7c011c8c55e0818
mlojek <mlojek@protonmail.com>
2024-11-06 14:23:25 +0100

    finish implementation of ObjectiveFunction class

--- src/sofes/objective_functions/objective_function.py
+++ src/sofes/objective_functions/objective_function.py
@@ -13,11 +13,27 @@ class ObjectiveFunction:
     """
 
     def __init__(self, name: str, dim: int) -> None:
-        """TODO"""
+        """
+        Class constructor.
+
+        :param name: name of the objective function
+        :param dim: dimensionality of the function
+        """
         self.name = name
         self.dim = dim
         self.num_calls = 0
 
     def __call__(self, x: List[float]) -> float:
-        """TODO"""
+        """
+        Evaluate a single point with the objective function.
+
+        :param x: point to be evaluated
+        :raises ValueError: if dimensionality of x doesn't match self.dim
+        :return: value of the function in the provided point
+        """
+        if not len(x) == self.dim:
+            raise ValueError(
+                f"The dimensionality of the provided point is not matching the dimensionality"
+                f"of the function. Expected {self.dim}, got {len(x)}"
+            )
         self.num_calls += 1

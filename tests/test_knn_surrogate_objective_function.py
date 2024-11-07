7a95d779c1727d38c216ada2a6a7360949caeb62
mlojek <mlojek@protonmail.com>
2024-11-07 13:55:51 +0100

    add docstrings to KNNSOF tests

--- tests/test_knn_surrogate_objective_function.py
+++ tests/test_knn_surrogate_objective_function.py
@@ -15,7 +15,10 @@ class TestKNNSurrogateObjectiveFunction:
     """
 
     def test_all_same(self):
-        """"""
+        """
+        Simple test case. All points have the same x and y and the result is expected to be
+        the same y.
+        """
         train_set = [([0], 1)] * 100
         knn_sof = KNNSurrogateObjectiveFunction(train_set, 5)
         y = knn_sof([100])
@@ -24,7 +27,10 @@ class TestKNNSurrogateObjectiveFunction:
         assert knn_sof.dim == 1
 
     def test_square(self):
-        """"""
+        """
+        Test case with 4 point that make up a square. Two pairs of point have values 1 and 3.
+        The x is the center of the square, so the expected y value is 2.
+        """
         train_set = [([1, 1], 1), ([1, -1], 3), ([-1, -1], 1), ([-1, 1], 3)]
         knn_sof = KNNSurrogateObjectiveFunction(train_set, 4)
         y = knn_sof([0, 0])
@@ -33,7 +39,9 @@ class TestKNNSurrogateObjectiveFunction:
         assert knn_sof.dim == 2
 
     def test_dim_not_constant(self):
-        """"""
+        """
+        Check if the initalization fails when x-s in train set have different dimensionality.
+        """
         train_set = [([1, 1], 1), ([1, -1], 3), ([-1, -1], 1), ([-1], 3)]
         with pytest.raises(ValueError):
             KNNSurrogateObjectiveFunction(train_set, 4)

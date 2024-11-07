11fda9bdd320e1c8776f1857af23fecbffdc2641
mlojek <mlojek@protonmail.com>
2024-11-07 18:03:00 +0100

    start implementation of ARM

--- src/sofes/metamodels/approximate_ranking_metamodel.py
+++ src/sofes/metamodels/approximate_ranking_metamodel.py
@@ -6,8 +6,19 @@ Approximate ranking metamodel based on lmm-CMA-ES.
 class ApproximateRankingMetamodel:
     """Approximate ranking metamodel based on lmm-CMA-ES"""
 
-    def __init__(self) -> None:
-        """placeholder"""
+    def __init__(self, input_size: int, popsize: int) -> None:
+        self.input_size = input_size
+        self.popsize = popsize
+
+        self.n_init = input_size
+        self.n_step = max(1, input_size // 10)
+
+        self.train_set = []
+
+        self.objective_function = None
+        self.surrogate_function = None
+
+
 
     def placeholder1(self) -> None:
         """placeholder"""

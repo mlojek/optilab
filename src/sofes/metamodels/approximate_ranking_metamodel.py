03b95b2f112413c92407da80a2dc25e75bd69411
mlojek <mlojek@protonmail.com>
2024-11-08 13:05:53 +0100

    resolve typing of ARM init args

--- src/sofes/metamodels/approximate_ranking_metamodel.py
+++ src/sofes/metamodels/approximate_ranking_metamodel.py
@@ -2,13 +2,21 @@
 Approximate ranking metamodel based on lmm-CMA-ES.
 """
 
-from typing import List
+from typing import List, Type
+
+from sofes.objective_functions import ObjectiveFunction, SurrogateObjectiveFunction
 
 
 class ApproximateRankingMetamodel:
     """Approximate ranking metamodel based on lmm-CMA-ES"""
 
-    def __init__(self, input_size: int, popsize: int) -> None:
+    def __init__(
+        self,
+        input_size: int,
+        popsize: int,
+        objective_function: ObjectiveFunction,
+        surrogate_function_type: Type[SurrogateObjectiveFunction],
+    ) -> None:
         self.input_size = input_size
         self.popsize = popsize
 
@@ -17,8 +25,8 @@ class ApproximateRankingMetamodel:
 
         self.train_set = []
 
-        self.objective_function = None
-        self.surrogate_function = None
+        self.objective_function = objective_function
+        self.surrogate_function = None  # initialize SOF
 
     def __call__(self, x, train_set) -> List[float]:
         # assert lambda

8a3b99d10d9bbbab3538b32d337e2d573361b960
mlojek <mlojek@protonmail.com>
2024-12-01 17:52:50 +0100

    Finished implementing all objective functions from lmm-cma-es

--- src/sofes/objective_functions/__init__.py
+++ src/sofes/objective_functions/__init__.py
@@ -2,8 +2,13 @@
 Component ObjectiveFunction. It's an object that describes an objective function to be optimized.
 """
 
+from .ackley_function import AckleyFunction
 from .cec2017_objective_function import CEC2017ObjectiveFunction
 from .knn_surrogate_objective_function import KNNSurrogateObjectiveFunction
+from .noisy_sphere_function import NoisySphereFunction
 from .objective_function import ObjectiveFunction
+from .rastrigin_function import RastriginFunction
+from .rosenbrock_function import RosenbrockFunction
+from .schwefel_function import SchwefelFunction
 from .sphere_function import SphereFunction
 from .surrogate_objective_function import SurrogateObjectiveFunction
---
+++ src/sofes/objective_functions/noisy_sphere_function.py
@@ -0,0 +1,38 @@
+"""
+The noisy sphere function objective function
+"""
+
+# pylint: disable=too-few-public-methods
+
+from typing import List
+
+import numpy as np
+
+from .objective_function import ObjectiveFunction
+
+
+class NoisySphereFunction(ObjectiveFunction):
+    """
+    Noisy sphere objective function.
+    """
+
+    def __init__(self, epsilon: float, dim: int):
+        """
+        Class constructor.
+
+        :raises ValueError: when the number of function is invalid.
+        :param dim: dimensionality of the function.
+        """
+        super().__init__(f"noisy sphere {epsilon}", dim)
+        self.epsilon = epsilon
+
+    def __call__(self, x: List[float]) -> float:
+        """
+        Evaluate a single point with the objective function.
+
+        :param x: point to be evaluated
+        :raises ValueError: if dimensionality of x doesn't match self.dim
+        :return: value of the function in the provided point
+        """
+        super().__call__(x)
+        return sum(x_i**2 for x_i in x) * (1 + np.random.normal(0, 1))

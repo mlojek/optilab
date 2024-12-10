7edbc3cbc50dfe8f1aee494f84f0d44fc4f8ef34
mlojek <mlojek@protonmail.com>
2024-12-10 09:57:58 +0100

    add basic sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,10 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../src/sofes'))
 
 project = 'optilab'
 copyright = '2024, Łojek'
@@ -14,7 +18,7 @@ release = '3'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']

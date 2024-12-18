37406a76cd05d6d4398e31d86383c79367c7b743
Marcin <mlojek@protonmail.com>
2024-12-18 23:36:00 +0100

    Start implementing Optimizer class

--- src/optilab/optimizers/optimizer.py
+++ src/optilab/optimizers/optimizer.py
@@ -2,6 +2,10 @@
 Base class for optimizers.
 """
 
+# pylint: disable=too-few-public-methods
+
+from typing import Any, Dict
+
 from ..data_classes import Bounds, OptimizationRun, OptimizerMetadata
 from ..functions import ObjectiveFunction
 
@@ -11,17 +15,16 @@ class Optimizer:
     Base class for optimizers.
     """
 
-    def __init__(self):
+    def __init__(
+        self, name: str, population_size: int, hyperparameters: Dict[str, Any]
+    ) -> None:
         """
         Class constructor.
         """
-
-    def get_metadata(self) -> OptimizerMetadata:
-        """
-        Get metadata describing the optimizer.
-        """
+        self.metadata = OptimizerMetadata(name, population_size, hyperparameters)
 
     def optimize(self, function: ObjectiveFunction, bounds: Bounds) -> OptimizationRun:
         """
         Optimize a certain objective function.
         """
+        raise NotImplementedError

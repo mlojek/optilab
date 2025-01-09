a5ff79d49b36fe346868264334850da2853c23ed
Marcin <mlojek@protonmail.com>
2025-01-09 21:31:18 +0100

    Use new cmaes optimizer in experiment 001

--- experiments/001_reproduce_lmm_cma_es/main.py
+++ experiments/001_reproduce_lmm_cma_es/main.py
@@ -14,18 +14,22 @@ from optilab.functions.multimodal import RosenbrockFunction
 from optilab.plotting import plot_ecdf_curves
 from optilab.data_classes import OptimizationRun, OptimizerMetadata
 from optilab.utils import dump_to_pickle
+from optilab.optimizers import CMAES
 
 if __name__ == "__main__":
     # hyperparams:
     DIM = 2
     POPSIZE = 6
-    NUM_RUNS = 20
+    NUM_RUNS = 51
     CALL_BUDGET = 1e4 * DIM
     TOL = 1e-10
 
     # optimized problem
     BOUNDS = Bounds(-10, 10)
     FUNC = CumulativeSquaredSums(DIM)
+
+    precooked = CMAES(POPSIZE, 1)
+    precooked_cmaes_run = precooked.run_optimization(NUM_RUNS, FUNC, BOUNDS, CALL_BUDGET, 0.0, TOL)
 
     # perform optimization with vanilla cmaes
     cmaes_logs = [
@@ -71,9 +75,11 @@ if __name__ == "__main__":
 
     # print stats
     vanilla_times = [len(log) for log in cmaes_logs]
+    precooked_times = [len(log) for log in precooked_cmaes_run.logs]
     lmm_times = [len(log) for log in lmm_cmaes_logs]
 
     print(f'vanilla {np.average(vanilla_times)} {np.std(vanilla_times)}')
+    print(f'precooked {np.average(precooked_times)} {np.std(precooked_times)}')
     print(f'lmm {np.average(lmm_times)} {np.std(lmm_times)}')
     
     # plot results
@@ -81,10 +87,11 @@ if __name__ == "__main__":
         {
             "cma-es": cmaes_logs,
             "lmm-cma-es": lmm_cmaes_logs,
+            'precooked': precooked_cmaes_run.logs
         },
         n_dimensions=DIM,
         savepath=f"ecdf_{FUNC.name}_{DIM}.png",
         allowed_error=TOL
     )
 
-    dump_to_pickle([cmaes_run, lmm_cmaes_run], f'lmm_reproduction_{FUNC.name}_{DIM}.pkl')
+    dump_to_pickle([cmaes_run, lmm_cmaes_run, precooked_cmaes_run], f'lmm_reproduction_{FUNC.name}_{DIM}.pkl')

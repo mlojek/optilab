7b376d055d21b2ff5c0c56449c4f02d997be378c
mlojek <mlojek@protonmail.com>
2025-01-26 09:25:13 +0000

    Allow bulk processing in CLI

--- src/optilab/__main__.py
+++ src/optilab/__main__.py
@@ -15,43 +15,56 @@ from .utils.pickle_utils import load_from_pickle
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Optilab CLI utility.")
     parser.add_argument(
-        "pickle_path", type=Path, help="Path to pickle file with optimization runs."
+        "pickle_path",
+        type=Path,
+        help="Path to pickle file or directory with optimization runs.",
     )
     parser.add_argument(
         "--hide_plots", action="store_true", help="Hide plots when running the script."
     )
     args = parser.parse_args()
 
-    filename_stem = args.pickle_path.stem
+    file_path_list = []
 
-    data = load_from_pickle(args.pickle_path)
+    if args.pickle_path.is_file():
+        file_path_list.append(args.pickle_path)
+    elif args.pickle_path.is_dir():
+        for file_path in sorted(args.pickle_path.iterdir()):
+            if file_path.is_file() and file_path.suffix == ".pkl":
+                file_path_list.append(file_path)
 
-    assert isinstance(data, list)
-    for run in data:
-        assert isinstance(run, OptimizationRun)
+    for file_path in file_path_list:
+        print(f"file {file_path}")
+        filename_stem = file_path.stem
 
-    plot_convergence_curve(
-        data={run.model_metadata.name: run.logs for run in data},
-        savepath=f"{filename_stem}.convergence.png",
-        show=not args.hide_plots,
-    )
+        data = load_from_pickle(file_path)
 
-    plot_ecdf_curves(
-        data={run.model_metadata.name: run.logs for run in data},
-        n_dimensions=data[0].function_metadata.dim,
-        n_thresholds=100,
-        allowed_error=data[0].tolerance,
-        savepath=f"{filename_stem}.ecdf.png",
-        show=not args.hide_plots,
-    )
+        assert isinstance(data, list)
+        for run in data:
+            assert isinstance(run, OptimizationRun)
 
-    plot_box_plot(
-        data={run.model_metadata.name: run.bests_y() for run in data},
-        savepath=f"{filename_stem}.box_plot.png",
-        show=not args.hide_plots,
-    )
+        plot_convergence_curve(
+            data={run.model_metadata.name: run.logs for run in data},
+            savepath=f"{filename_stem}.convergence.png",
+            show=not args.hide_plots,
+        )
 
-    stats = pd.concat([run.stats() for run in data], ignore_index=True)
+        plot_ecdf_curves(
+            data={run.model_metadata.name: run.logs for run in data},
+            n_dimensions=data[0].function_metadata.dim,
+            n_thresholds=100,
+            allowed_error=data[0].tolerance,
+            savepath=f"{filename_stem}.ecdf.png",
+            show=not args.hide_plots,
+        )
 
-    stats.to_csv(f"{filename_stem}.stats.csv")
-    print(tabulate(stats, headers="keys", tablefmt="github"))
+        plot_box_plot(
+            data={run.model_metadata.name: run.bests_y() for run in data},
+            savepath=f"{filename_stem}.box_plot.png",
+            show=not args.hide_plots,
+        )
+
+        stats = pd.concat([run.stats() for run in data], ignore_index=True)
+
+        stats.to_csv(f"{filename_stem}.stats.csv")
+        print(tabulate(stats, headers="keys", tablefmt="github"))

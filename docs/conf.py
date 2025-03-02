4a1deabb2a97e8cb9c2800bb62f0436af82d8194
Marcin <mlojek@protonmail.com>
2025-03-02 21:27:51 +0100

    Bump version to 20

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../src/optilab'))
 project = 'optilab'
 copyright = '2025, Marcin Łojek'
 author = 'Marcin Łojek'
-release = '19'
+release = '20'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration

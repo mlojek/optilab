f77c60510ca3bafe65216ca22a3acd2d96c1d04d
mlojek <mlojek@protonmail.com>
2025-03-24 09:48:10 +0100

    Add CLI arguments for aggregation and sort flag args alphabetically

--- src/optilab/__main__.py
+++ src/optilab/__main__.py
@@ -23,21 +23,14 @@ if __name__ == "__main__":
         help="Path to pickle file or directory with optimization runs.",
     )
     parser.add_argument(
-        "--save_path",
-        type=Path,
-        default=Path.cwd(),
-        help="Path to directory to save the artifacts. Default is the user's working directory.",
-    )
-    parser.add_argument(
-        "--hide_plots", action="store_true", help="Hide plots when running the script."
-    )
-    parser.add_argument(
-        "--test_y", action="store_true", help="Perform Mann-Whitney U test on y values."
-    )
-    parser.add_argument(
-        "--test_evals",
+        "--aggregate_pvalue",
         action="store_true",
-        help="Perform Mann-Whitney U test on eval values.",
+        help="Aggregate pvalues of stat tests against run 0 in each pickle file into one table.",
+    )
+    parser.add_argument(
+        "--aggregate_stats",
+        action="store_true",
+        help="Aggregate median and iqr for all processed runs into one table.",
     )
     parser.add_argument(
         "--entries",
@@ -46,19 +39,36 @@ if __name__ == "__main__":
         help="Space separated list of indexes of entries to include in analysis.",
     )
     parser.add_argument(
-        "--raw_values",
-        action="store_true",
-        help="If specified, y values below tolerance are not substituted by tolerance value.",
-    )
-    parser.add_argument(
         "--hide_outliers",
         action="store_true",
         help="If specified, outliers will not be shown in the box plot.",
     )
     parser.add_argument(
+        "--hide_plots", action="store_true", help="Hide plots when running the script."
+    )
+    parser.add_argument(
         "--no_save",
         action="store_true",
         help="If specified, no artifacts will be saved.",
+    )
+    parser.add_argument(
+        "--raw_values",
+        action="store_true",
+        help="If specified, y values below tolerance are not substituted by tolerance value.",
+    )
+    parser.add_argument(
+        "--save_path",
+        type=Path,
+        default=Path.cwd(),
+        help="Path to directory to save the artifacts. Default is the user's working directory.",
+    )
+    parser.add_argument(
+        "--test_evals",
+        action="store_true",
+        help="Perform Mann-Whitney U test on eval values.",
+    )
+    parser.add_argument(
+        "--test_y", action="store_true", help="Perform Mann-Whitney U test on y values."
     )
     args = parser.parse_args()
 

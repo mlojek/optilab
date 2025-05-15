e785972546ae87c7f1f8277d3ba1311c77693b83
mlojek <mlojek@protonmail.com>
2025-05-15 20:55:38 +0200

    Remove version from docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@ sys.path.insert(0, os.path.abspath('../src/optilab'))
 project = 'optilab'
 copyright = '2025, Marcin Łojek'
 author = 'Marcin Łojek'
-release = '22'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
